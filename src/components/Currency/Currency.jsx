abf36c4dfe712f33b9e6e7f96095d96d30073d6e
Max <maxminullin@gmail.com>
2021-08-25 18:05:03 +0300

    add markdown

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -3,30 +3,55 @@ import { fetchData } from './fetchData'
 import style from './Currency.scss'
 
 const Currency = () => {
+
   const [USD, setUSD] = useState({ buy: '00.00', sale: '00.00' })
   const [EUR, setEUR] = useState({ buy: '00.00', sale: '00.00' })
   const [RUB, setRUB] = useState({ buy: '00.00', sale: '00.00' })
-  
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     setLoading(true)
-    
     fetchData()
-      .then(res => res.data)
+      .then(res => {
+        res.data.forEach(item => {
+          if (item.ccy === 'USD') {
+            setUSD({ buy: item.buy, sale: item.sale })
+          } else if (item.ccy === 'EUR') {
+            setEUR({ buy: item.buy, sale: item.sale })
+          } else if (item.ccy === 'RUR') {
+            setRUB({ buy: item.buy, sale: item.sale })
+          }
+        })
+      })
       .catch(err => err)
-      .finally(setLoading(false))
-    
-  },[])
+      .finally(setLoading(false))}
+    ,[])
 
   return (
-    <p>hello</p>
+    <div>
+      <ul className={style.titleList}>
+        <li className={style.title}>Валюта</li>
+        <li className={style.title}>Покупка</li>
+        <li className={style.title}>Продажа</li>
+      </ul>
+      <ul>
+        <li className={style.currencyName}>USD</li>
+        <li className={style.price}>{ USD.buy }</li>
+        <li className={style.price}>{ USD.sale}</li>
+      </ul>
+      <ul>
+        <li className={style.currencyName}>EUR</li>
+        <li className={style.price}>{ EUR.buy }</li>
+        <li className={style.price}>{ EUR.sale }</li>
+      </ul>
+      <ul>
+        <li className={style.currencyName}>RUB</li>
+        <li className={style.price}>{ RUB.buy }</li>
+        <li className={style.price}>{ RUB.sale }</li>
+      </ul>
+   </div>
   )
 }
 
-
-Currency.prototype = {
-  
-}
 
 export default Currency
