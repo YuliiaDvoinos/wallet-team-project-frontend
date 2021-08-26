b9cd0393aa8c71644f1213b3b9bebad013a217f9
Andrey Klemenko <web.klemenko@gmail.com>
2021-08-27 01:14:44 +0300

    create get transactions operations/actions

--- src/components/HomeTab/HomeTabMobile.jsx
+++ src/components/HomeTab/HomeTabMobile.jsx
@@ -1,33 +1,77 @@
 import './HomeTabMobile.scss';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import operations from '../../redux/transactions/transactions-operations';
+import transactionSelectors from '../../redux/transactions/transactions-selectors';
+// import {}
+const { fetchTransactions } = operations;
+const { getAllTransactions } = transactionSelectors;
 
 function HomeTabMobile() {
+  const transactions = useSelector(getAllTransactions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setTimeout(() => dispatch(fetchTransactions()));
+  }, [dispatch]);
+
   return (
-    <div className="home-table__card">
-      <div className="home-table__group">
-        <p className="home-table__item">Дата</p>
-        <p className="home-table__item_value">10.08.19</p>
-      </div>
-      <div className="home-table__group">
-        <p className="home-table__item">Тип</p>
-        <p className="home-table__item_value">-</p>
-      </div>
-      <div className="home-table__group">
-        <p className="home-table__item">Категория</p>
-        <p className="home-table__item_value">Разное</p>
-      </div>
-      <div className="home-table__group">
-        <p className="home-table__item">Комментарий</p>
-        <p className="home-table__item_value">Подарок жене</p>
-      </div>
-      <div className="home-table__group">
-        <p className="home-table__item">Сума</p>
-        <p className="home-table__item_value home-table__item_spend">300</p>
-      </div>
-      <div className="home-table__group">
-        <p className="home-table__item">Баланс</p>
-        <p className="home-table__item_value">6 900.00</p>
-      </div>
-    </div>
+    <ul className="home-table__card">
+      {transactions.map(({ _id, type, date, money, category, comment }) => (
+        <>
+          <ul key={_id} className="home-table__group list">
+            <li className="home-table__item">Дата</li>
+            <li className="home-table__item_value">{date}</li>
+          </ul>
+          <ul className="home-table__group list">
+            <li className="home-table__item">Тип</li>
+            <li className="home-table__item_value">{type}</li>
+          </ul>
+          <ul className="home-table__group list">
+            <li className="home-table__item">Категория</li>
+            <li className="home-table__item_value">{category.name}</li>
+          </ul>
+          <ul className="home-table__group list">
+            <li className="home-table__item">Комментарий</li>
+            <li className="home-table__item_value">{comment}</li>
+          </ul>
+          <ul className="home-table__group list">
+            <li className="home-table__item">Сума</li>
+            <li className="home-table__item_value home-table__item_spend">
+              {money}
+            </li>
+          </ul>
+          <ul className="home-table__group list">
+            <li className="home-table__item">Баланс</li>
+            <li className="home-table__item_value">6 900.00</li>
+          </ul>
+        </>
+      ))}
+      {/* <ul className="home-table__group list">
+        <li className="home-table__item">Дата</li>
+        <li className="home-table__item_value">10.08.19</li>
+      </ul>
+      <ul className="home-table__group list">
+        <li className="home-table__item">Тип</li>
+        <li className="home-table__item_value">-</li>
+      </ul>
+      <ul className="home-table__group list">
+        <li className="home-table__item">Категория</li>
+        <li className="home-table__item_value">Разное</li>
+      </ul>
+      <ul className="home-table__group list">
+        <li className="home-table__item">Комментарий</li>
+        <li className="home-table__item_value">Подарок жене</li>
+      </ul>
+      <ul className="home-table__group list">
+        <li className="home-table__item">Сума</li>
+        <li className="home-table__item_value home-table__item_spend">300</li>
+      </ul>
+      <ul className="home-table__group list">
+        <li className="home-table__item">Баланс</li>
+        <li className="home-table__item_value">6 900.00</li>
+      </ul> */}
+    </ul>
   );
 }
 
---
+++ src/redux/transactions/transactions-operations.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import * as actions from './transactions-actions';
+
+axios.defaults.baseURL = 'http://localhost:4000/api';
+
+const fetchTransactions = () => async dispatch => {
+  dispatch(actions.getTransactionsRequest());
+
+  try {
+    const { data } = await axios.get('/transactions');
+    dispatch(actions.getTransactionsSuccess(data.data.result));
+    console.log(data);
+  } catch (error) {
+    dispatch(actions.getTransactionsError());
+    alert({
+      text: "You don't have transactions",
+      type: 'error',
+    });
+  }
+};
+
+const operations = {
+  fetchTransactions,
+};
+
+export default operations;
