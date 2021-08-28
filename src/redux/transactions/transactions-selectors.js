f296f5717c968c45ec76a7d1b7a3a340ded2cfdf
deoneshka <oleg.deonega@gmail.com>
2021-08-28 18:25:17 +0300

    done
    Переписал модалку на хуки, можно переиспользовать теперь. Сделал верстку на десктоп и планшет модалки transaction (поправлю scss следующим коммитом). Исправил не точности по button transaction и подключил к redux store ее. Пакеты установите)

--- src/redux/transactions/transactions-selectors.js
+++ src/redux/transactions/transactions-selectors.js
@@ -2,8 +2,12 @@
 
 const getAllTransactions = state => state.transactions.result;
 
+const getIsModalAddTransactionOpen = state =>
+  state.transactions.modalTransaction;
+
 const transactionSelectors = {
   getAllTransactions,
+  getIsModalAddTransactionOpen,
 };
 
 export default transactionSelectors;
