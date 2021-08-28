7ba828127432822ce15d303726a4939ea5a09f65
Max <maxminullin@gmail.com>
2021-08-28 15:17:27 -0700

    fix style navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,39 +6,39 @@ import './Navigation.scss';
 export default function Navigation() {
   return (
     <div className="container">
-      <ul className="nav-list">
-        <li className="nav-item">
+      <ul className="nav list">
+        <li className="nav__item">
           <NavLink
             to={routes.home}
-            className="nav-link"
-            activeClassName="active-link"
+            className="nav__link link"
+            activeClassName="nav__link--active"
           >
-            <span className="nav-icon">
-              <Icons id="home" className="nav-icon-svg" />
+            <span className="nav__icon">
+              <Icons id="home" className="nav__icon--svg" />
             </span>
-            <p>Главная</p>
+            <p className="nav__link--text">Главная</p>
           </NavLink>
         </li>
-        <li className="nav-item">
+        <li className="nav__item">
           <NavLink
             to={routes.diagram}
-            className="nav-link"
-            activeClassName="active-link"
+            className="nav__link link"
+            activeClassName="nav__link--active"
           >
-            <span className="nav-icon">
-              <Icons id="diagram" className="nav-icon-svg" />
+            <span className="nav__icon">
+              <Icons id="diagram" className="nav__icon--svg" />
             </span>
-            <p>Статистика</p>
+            <p className="nav__link--text">Статистика</p>
           </NavLink>
         </li>
-        <li className="nav-item">
+        <li className="nav__item">
           <NavLink
             to="/currency"
-            className="mobile-btn"
-            activeClassName="active-link"
+            className="nav__link--mobile-btn"
+            activeClassName="nav__link--active"
           >
-            <span className="nav-icon">
-              <Icons id="currency" className="nav-icon-svg" />
+            <span className="nav__icon">
+              <Icons id="currency" className="nav__icon--svg" />
             </span>
           </NavLink>
         </li>
