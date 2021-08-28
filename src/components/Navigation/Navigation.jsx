00e49676714266989cef7bf4c0a09397591f962a
Dizardmk <Dizardmk@gmail.com>
2021-08-28 14:19:00 +0300

    adds fix to hometab, nav and dashboard

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,44 +5,42 @@ import './Navigation.scss';
 
 export default function Navigation() {
   return (
-    <div className="container">
-      <ul className="nav-list">
-        <li className="nav-item">
-          <NavLink
-            to={routes.home}
-            className="nav-link"
-            activeClassName="active-link"
-          >
-            <span className="nav-icon">
-              <SvgGenerator id="home" />
-            </span>
-            <p>Главная</p>
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to={routes.diagram}
-            className="nav-link"
-            activeClassName="active-link"
-          >
-            <span className="nav-icon">
-              <SvgGenerator id="diagram" />
-            </span>
-            <p>Статистика</p>
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/currency"
-            className="mobile-btn"
-            activeClassName="active-link"
-          >
-            <span className="nav-icon">
-              <SvgGenerator id="currency" />
-            </span>
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <ul className="nav-list list">
+      <li className="nav-item">
+        <NavLink
+          to={routes.dashboard}
+          className="nav-link"
+          activeClassName="active-link link"
+        >
+          <span className="nav-icon">
+            <SvgGenerator id="home" />
+          </span>
+          <p>Главная</p>
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to={routes.statistics}
+          className="nav-link"
+          activeClassName="active-link"
+        >
+          <span className="nav-icon">
+            <SvgGenerator id="diagram" />
+          </span>
+          <p>Статистика</p>
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="/currency"
+          className="mobile-btn"
+          activeClassName="active-link"
+        >
+          <span className="nav-icon">
+            <SvgGenerator id="currency" />
+          </span>
+        </NavLink>
+      </li>
+    </ul>
   );
 }
