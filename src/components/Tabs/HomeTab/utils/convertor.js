d9de6a05e65a24f28d2801b1d37e7fa85dcee77c
Dizardmk <Dizardmk@gmail.com>
2021-08-30 19:49:29 +0300

    adds fix to transactions sort

--- src/components/Tabs/HomeTab/utils/convertor.js
+++ src/components/Tabs/HomeTab/utils/convertor.js
@@ -1,29 +1,31 @@
 const convertor = array =>
   array
-    .map(({ _id, type, date, money, category, comment, balance }) => {
-      //type
-      let convertedType;
-      if (type === 'income') {
-        convertedType = '+';
-      } else {
-        convertedType = '-';
-      }
+    .map(
+      ({ _id, type, date, money, category, comment, balance, createdAt }) => {
+        //type
+        let convertedType;
+        if (type === 'income') {
+          convertedType = '+';
+        } else {
+          convertedType = '-';
+        }
 
-      //date
-      const dateArr = [...date];
-      dateArr.splice(6, 2);
+        //date
+        const dateArr = [...date];
+        dateArr.splice(6, 2);
 
-      const obj = {
-        _id,
-        date: dateArr.join(''),
-        type: convertedType,
-        money,
-        category,
-        comment,
-        balance,
-      };
-      return obj;
-    })
-    .sort((a, b) => (a.date < b.date ? 1 : -1));
+        const obj = {
+          _id,
+          date: dateArr.join(''),
+          type: convertedType,
+          money,
+          category,
+          comment,
+          balance,
+        };
+        return obj;
+      },
+    )
+    .sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1));
 
 export default convertor;
