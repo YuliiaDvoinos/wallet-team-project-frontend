1c2398993d4f828dcab20667e845d452c9a1ed44
Yuliia Dvoinos <69351064+YuliiaDvoinos@users.noreply.github.com>
2021-08-30 20:39:22 +0300

    remove duplicate

--- src/redux/transactions/transactions-selectors.js
+++ src/redux/transactions/transactions-selectors.js
@@ -2,8 +2,7 @@ const getAllTransactions = state => state.transactions.allTransactions;
 
 const getAddedTransactions = state => state.transactions.addedTransactions;
 
-const getAllTransactions = state => state.transactions.result;
-const getStats = state => state.transactions.result;
+
 const getIsModalAddTransactionOpen = state =>
   state.transactions.modalTransaction;
 const getStats = state => state.transactions.result;
