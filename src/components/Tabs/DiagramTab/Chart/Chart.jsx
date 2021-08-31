7cd96c20adb0bdd3e44c63b782c244d8d1afdddd
Yevhenii Blynnyi <v-yblynnyi@expediagroup.com>
2021-08-31 22:31:59 +0300

    add Doughnut component

--- src/components/Tabs/DiagramTab/Chart/Chart.jsx
+++ src/components/Tabs/DiagramTab/Chart/Chart.jsx
@@ -1,13 +1,45 @@
 import './Chart.scss';
+import { Doughnut } from 'react-chartjs-2';
+export default function Chart({ statistics }) {
+  const data = {
+    datasets: [
+      {
+        label: '# of Votes',
+        data: statistics.categoriesSummary
+          ? Object.values(statistics.categoriesSummary)
+          : null,
 
-export default function Chart({ statistics: { totalSpend } }) {
+        backgroundColor: [
+          '#FED057',
+          '#FFD8D0',
+          '#FD9498',
+          '#C5BAFF',
+          '#6E78E8',
+          '#4A56E2',
+          '#81E1FF',
+          '#24CCA7',
+          '#00AD84',
+        ],
+        borderColor: [
+          '#FED057',
+          '#FFD8D0',
+          '#FD9498',
+          ' #C5BAFF',
+          '#6E78E8',
+          '#4A56E2',
+          '#81E1FF',
+          '#24CCA7',
+          '#00AD84',
+        ],
+        borderWidth: 1,
+        cutout: 99,
+      },
+    ],
+  };
   return (
-    <>
-      <div className="chart">
-        <div className="chart__contents">
-          <span className="chart__ballance">&#8372; {totalSpend}</span>
-        </div>
-      </div>
-    </>
+    <div className="chart">
+      <Doughnut className="chart__contents" data={data} />
+      <span className="chart__ballance">&#8372; {statistics.totalSpend}</span>
+    </div>
   );
 }
